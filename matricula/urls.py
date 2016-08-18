fe5c2f287fba2026caf0a15309c7e9c15a0d04f3
Luis Zárate <luis.zarate@solvosoft.com>
2016-08-18 11:29:17 -0600

    Updating requirements and urls for django=1.10

--- matricula/urls.py
+++ matricula/urls.py
@@ -5,34 +5,30 @@ Created on 7/4/2015
 @author: luisza
 '''
 
-from django.conf.urls import patterns, url, include
-from matricula.views.Auth import StudentEdit
-
+from django.conf.urls import url, include
+from matricula.views.Auth import *
+from matricula.views.Courses import *
 from .admin import admin_site
 from matricula.views.Pages import PageDetail
+from matricula.views.Enrollments import *
 
-urlpatterns = patterns('matricula.views.Auth',
-                       url('^create_user$', 'create_user', name="create_user"),
-                       url('^confirm_email$', 'confirm_email', name="confirm_email"),
-                       url('^authenticate$', 'authenticate', name="authenticate"),
-                       url('^logout$', 'logout', name="logout"),
-                       url('^recover_password$', 'recover_password', name="recover_password"),
-                       url('^mail_recover_pass$', 'mail_recover_pass', name='mail_recover_pass'),
-                       url('^user/profile/(?P<pk>[0-9]+)/$', StudentEdit.as_view(), name='myprofile'),
-                       )
+urlpatterns = [
+    url('^create_user$', create_user, name="create_user"),
+    url('^confirm_email$', confirm_email, name="confirm_email"),
+    url('^authenticate$', authenticate, name="authenticate"),
+    url('^logout$', logout, name="logout"),
+    url('^recover_password$', recover_password, name="recover_password"),
+    url('^mail_recover_pass$', mail_recover_pass, name='mail_recover_pass'),
+    url('^user/profile/(?P<pk>[0-9]+)/$', StudentEdit.as_view(), name='myprofile'),
+    url('^courses$', list_courses, name='courses'),
+    url('^course/(?P<pk>\d+)$', view_course, name='course'),
 
-urlpatterns += patterns('matricula.views.Courses',
-                       url('^courses$', 'list_courses', name="courses"),
-                       url('^course/(?P<pk>\d+)$', 'view_course', name="course"),
-                       )
 
-urlpatterns += patterns('matricula.views.Enrollments',
-                        url('^enrollme/(?P<pk>\d+)$', 'enrollme', name="enrollme"),
-                        url('^enrollment$', 'list_enroll', name="enrollment"),
-                        url('^finish_enroll/(?P<pk>\d+)$', 'finish_enroll', name="finish_enroll"),
-                        )
+    url('^enrollme/(?P<pk>\d+)$', enrollme, name="enrollme"),
+    url('^enrollment$', list_enroll, name="enrollment"),
+    url('^finish_enroll/(?P<pk>\d+)$', finish_enroll, name="finish_enroll"),
 
-urlpatterns += patterns('',
-                        url(r'^admin/', include(admin_site.urls)),
-                        url(r'^pages/(?P<pk>\d+)$', PageDetail.as_view(), name="academica_pages"),
-                        )
+
+    url(r'^admin/', include(admin_site.urls)),
+    url(r'^pages/(?P<pk>\d+)$', PageDetail.as_view(), name="academica_pages"),
+               ]
