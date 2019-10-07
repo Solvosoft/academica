1210feb9f5e26081b5e9d213a295b03a78f72596
Fiio_Hdez <fiorella.hernandez@ucr.ac.cr>
2019-10-07 16:51:37 -0600

    remove create membership from template option on contact admin model changes to the membership_add (change_form.html) template and add cbx to choose a membership template tests on cbx function of the membership_add template

--- membership_manager/admin.py
+++ membership_manager/admin.py
@@ -7,13 +7,15 @@ from django.utils.html import format_html
 
 from membership_core.models import MembershipTemplate
 from membership_manager import models
+from membership_manager.models import Membership
 
 admin.site.register([models.Invoice])
+
 
 class ContactAdmin(admin.ModelAdmin):
     list_filter = ('active', 'country')
     search_fields = ('first_name', 'last_name')
-    #list_editable = ('active',)
+    # list_editable = ('active',)
     list_display = ("first_name",
                     "last_name",
                     "email",
@@ -22,7 +24,7 @@ class ContactAdmin(admin.ModelAdmin):
                     "organizations",
                     "memberships",
                     "create_membership_template",
-                    "active" )
+                    "active")
 
     fields = [
         "first_name",
@@ -46,35 +48,36 @@ class ContactAdmin(admin.ModelAdmin):
                <a href="{}"  class="grp-button grp-button-state-inactive" >{}</a> - 
                <a href="{}" class="grp-button grp-button-state-active" target="_blank">Add</a>
             """,
-            reverse("admin:membership_manager_organization_changelist")+
-            "?contact="+str(obj.pk),
+            reverse("admin:membership_manager_organization_changelist") +
+            "?contact=" + str(obj.pk),
             obj.organization_set.count(),
             reverse("admin:membership_manager_organization_add") +
             "?contact=" + str(obj.pk) +
-            "&currency=" + str(obj.currency.pk) +"&"+
-            "&".join([x+"="+str(getattr(obj, x)) for x in ("email",
-                                    "cellphone",
-                                    "country",
-                                    "city",
-                                    "province",
-                                    "postal_code",
-                                    "payment_method",)])
+            "&currency=" + str(obj.currency.pk) + "&" +
+            "&".join([x + "=" + str(getattr(obj, x)) for x in ("email",
+                                                               "cellphone",
+                                                               "country",
+                                                               "city",
+                                                               "province",
+                                                               "postal_code",
+                                                               "payment_method",)])
         )
-    organizations.short_description = "Organizations"
 
+    organizations.short_description = "Organizations"
 
     def memberships(self, obj):
         return format_html(
             """<a href="{}" class="grp-button grp-button-state-inactive"  >{}</a> - 
                <a href="{}" class="grp-button grp-button-state-inactive" target="_blank">Add</a>
             """,
-            reverse("admin:membership_manager_membership_changelist")+
-            "?contact="+str(obj.pk),
+            reverse("admin:membership_manager_membership_changelist") +
+            "?contact=" + str(obj.pk),
             obj.membership_set.filter(state="active").count(),
             reverse("admin:membership_manager_membership_add") +
-               "?contact=" + str(obj.pk) + "&membership_type=Personal&currency=" +
-               str(obj.currency.pk)
-                    )
+            "?contact=" + str(obj.pk) + "&membership_type=Personal&currency=" +
+            str(obj.currency.pk)
+        )
+
     memberships.short_description = "Membership"
 
     def create_membership_template(self, obj):
@@ -85,26 +88,34 @@ class ContactAdmin(admin.ModelAdmin):
                 'templates': MembershipTemplate.objects.all()
             }
         )
+
     create_membership_template.short_description = "Create Membership from template"
+
 
 class MembershipRenewAdmin(admin.StackedInline):
     model = models.MembershipRenew
     extra = 0
 
+
 class MemberShipAdmin(admin.ModelAdmin):
     list_filter = ('state', 'contact__country', 'services')
     search_fields = ('contact__first_name', 'contact__last_name')
-    list_display = ('name', 'contact',  'annual_cost',
+    list_display = ('name', 'contact', 'annual_cost',
                     'currency', 'renewal_period', 'state')
     filter_horizontal = ['services']
     inlines = [MembershipRenewAdmin]
+
+    def changeform_view(self, request, obj_id, form_url, extra_context=None):
+        query = Membership.objects.all()
+        extra_context = {'data': query}
+        return super(MemberShipAdmin, self).changeform_view(request, obj_id, form_url, extra_context=extra_context)
 
 
 class OrganizationAdmin(admin.ModelAdmin):
     list_filter = ('active', 'country')
     search_fields = ('name', 'initials')
-    list_display = ( "name", "email", "cellphone",
-            "contact",   "memberships", "active" )
+    list_display = ("name", "email", "cellphone",
+                    "contact", "memberships", "active")
     fields = [
         "name",
         "initials",
@@ -122,12 +133,13 @@ class OrganizationAdmin(admin.ModelAdmin):
         "contact"
     ]
 
-
     def memberships(self, obj):
         return format_html('<a href="{}" target="_blank">{}</a>',
-                    "#", obj.membership_set.filter(state="active").count()
-                    )
+                           "#", obj.membership_set.filter(state="active").count()
+                           )
+
     memberships.short_description = "Membership"
+
 
 admin.site.register(models.Organization, OrganizationAdmin)
 admin.site.register(models.Contact, ContactAdmin)
