173823d57428019cedbb0a5bce54be73eb504e78
Luis Zárate M <luis.zarate@solvosoft.com>
2019-10-24 00:13:49 -0600

    Moving task to utils and pass timezone.now() as parameter

--- src/membership_manager/task_utils.py
+++ src/membership_manager/task_utils.py
@@ -1,13 +1,11 @@
-from decimal import Decimal
+from datetime import timedelta
 
 from async_notifications.utils import send_email_from_template
-from django.utils import timezone
 
-from membership_manager.admin_memberships import filter_memb_queryset
+from membership_manager.admin_pdf import invoice_expiration_filter_queryset
+from membership_manager.models import Invoice, MembershipRenew
+from membership_manager.render_pdf import generate_invoice
 
-
-from membership_manager.admin_pdf import invoice_expiration_filter_queryset, renewal_expiration_filter_manager
-from membership_manager.models import Membership, Invoice, MembershipRenew
 
 def notify_invoice_expiration(now):
     """
@@ -28,3 +26,69 @@ def notify_invoice_expiration(now):
                                  enqueued=True,
                                  user=None,
                                  upfile=None)
+
+
+def invoice_creation(now):
+    renews = MembershipRenew.objects.filter(end_date__lte=now + timedelta(days=60),
+                                            active=True,
+                                            graceperiod=False,
+                                            membership__state="active",
+                                            inv_m_renews=None
+                                            )
+    for renew in renews:
+        invoice = Invoice.objects.create(creation_date=now,
+                                         expiration_date=renew.end_date,
+                                         payment_date=renew.end_date,
+                                         membership=renew.membership,
+                                         renewal_period=renew,
+                                         description=f'{renew.membership.name} expira al {renew.end_date}. Debe ser pagada.',
+                                         amount=renew.membership.annual_cost,
+                                         currency=renew.membership.currency,
+                                         status='pending')
+        generate_invoice(invoice.membership, invoice, email_template="notification_mail", enqueued=True)
+
+
+def renew_graceperiod(now):
+    renews = MembershipRenew.objects.filter(end_date=now,
+                                            active=True,
+                                            graceperiod=False,
+                                            membership__state="active"
+                                            )
+
+    for renew in renews:
+        membership = renew.membership
+        MembershipRenew.objects.create(
+            creation_date=now,
+            membership=renew.membership,
+            start_date=now,
+            end_date=now + timedelta(days=60),
+            graceperiod=True,
+            active=True
+        )
+        membership.state = "graceperiod"
+        membership.save()
+
+
+def membership_deactivating(now):
+    renews = MembershipRenew.objects.filter(end_date=now,
+                                            active=True,
+                                            graceperiod=True,
+                                            membership__state="graceperiod"
+                                            )
+    for renew in renews:
+        membership = renew.membership
+        if membership.contact:
+            email = membership.contact.email
+        else:
+            email = membership.organization.contact.email
+        send_email_from_template('expiration_mail', [email],
+                                 context={
+                                     'membership': membership
+                                 },
+                                 enqueued=True,
+                                 user=None,
+                                 upfile=None)
+
+        membership.state = "inactive"
+        membership.save()
+        MembershipRenew.objects.filter(membership=membership).update(state="inactive")
--- src/membership_manager/tasks.py
+++ src/membership_manager/tasks.py
@@ -1,108 +1,23 @@
-from decimal import Decimal
-
-from async_notifications.utils import send_email_from_template
 from django.utils import timezone
 
-from membership_manager.admin_memberships import filter_memb_queryset
-from membership_manager.task_utils import notify_invoice_expiration
+from membership_manager.task_utils import notify_invoice_expiration, invoice_creation, renew_graceperiod, \
+    membership_deactivating
 from membresias_codigosur.celery import app
 
-from membership_manager.admin_pdf import invoice_expiration_filter_queryset, renewal_expiration_filter_manager
-from membership_manager.models import Membership, Invoice, MembershipRenew
 
-
-@app.task(name='task_notify_invoice')
+@app.task
 def task_notify_invoice_expiration():
     notify_invoice_expiration(timezone.now())
 
-
-def task_notify_membership_expiration():
-    """
-    Gets the membership , and notify is there is in the expiration range.
-    """
-    qset = Invoice.objects.all()
-    notify_qset = filter_memb_queryset(qset)
-
-
-@app.task(name='task_invoice_creation')
+@app.task
 def task_invoice_creation():
     """
     Create a invoice, at 60 days left - renewal expiration.
     """
-    qset = MembershipRenew.objects.all()  # queryset
-    # get the memberships with 30 days to expire (this is cuz if there is any membership with 1moth renewal
-    # period then we can handle it and create a invoice)
-    four_weeks_to_expire_qset = renewal_expiration_filter_manager(qset, 30)
-    # get memberships with 60 days to expire then check if there is any invoice with his pk and pending, then create
-    # one,
-    eight_weeks_to_expire_qset = renewal_expiration_filter_manager(qset, 60)
-    if four_weeks_to_expire_qset:
-        for renew in four_weeks_to_expire_qset:
-            tmp_checker = Invoice.objects.filter(renewal_period__id=renew.pk, status='pending')
-            if len(tmp_checker) == 0:
-                Invoice.objects.create(creation_date=timezone.now(),
-                                       expiration_date=renew.end_date,
-                                       payment_date=renew.end_date,
-                                       membership=renew.membership,
-                                       renewal_period=renew,
-                                       description=f'{renew.membership.name} expire at {renew.end_date}. Must pay.',
-                                       amount=Decimal(renew.membership.annual_cost/renew.membership.renewal_period.months),
-                                       currency=renew.membership.currency,
-                                       status='pending')
-    if eight_weeks_to_expire_qset:
-        for renew in eight_weeks_to_expire_qset:
-            tmp_checker = Invoice.objects.filter(renewal_period__id=renew.pk, status='pending')
-            if len(tmp_checker) == 0:
-                Invoice.objects.create(creation_date=timezone.now(),
-                                       expiration_date=renew.end_date,
-                                       payment_date=renew.end_date,
-                                       membership=renew.membership,
-                                       renewal_period=renew,
-                                       description=f'{renew.membership.name} expire at {renew.end_date}. Must pay.',
-                                       amount=renew.membership.annual_cost,
-                                       currency=renew.membership.currency,
-                                       status='pending')
+    invoice_creation(timezone.now())
 
 
-@app.task(name='task_deactivating_or_graceperiod')
+@app.task
 def task_membership_deactivating_or_graceperiod():
-    # this method will check if there is any invoices expired , then change them to graceperiod
-    yesterday_date = timezone.now() - timezone.timedelta(days=1)
-    grace_period_date = timezone.now() - timezone.timedelta(days=15)
-    qset = Invoice.objects.filter(status='pending')  # queryset
-
-    for invoice in qset:
-        tmp_memb_renew = MembershipRenew.objects.get(pk=invoice.renewal_period.pk)
-        # check if renewal graceperiod expire
-        if bool(tmp_memb_renew.graceperiod) and tmp_memb_renew.end_date.date() == grace_period_date.date():
-            # change membership to inactive
-            tmp_membship = Membership.objects.get(pk=invoice.membership.pk)
-            tmp_membship.state = 'inactive'
-            tmp_membship.save()
-            # now change renewal status
-            tmp_memb_renew.graceperiod = False
-            tmp_memb_renew.save()
-
-            # this notifies that membership has expired
-            if tmp_membship.contact:
-                email = tmp_membship.contact.email
-            else:
-                email = tmp_membship.organization.contact.email
-            send_email_from_template('expiration_mail', [email],
-                                     context={
-                                         'membership': tmp_membship
-                                     },
-                                     enqueued=True,
-                                     user=None,
-                                     upfile=None)
-
-        # check if renewal expire
-        if bool(tmp_memb_renew.active) and tmp_memb_renew.end_date.date() == yesterday_date.date():
-            # now change membership status
-            tmp_membship = Membership.objects.get(pk=invoice.membership.pk)
-            tmp_membship.state = 'graceperiod'
-            tmp_membship.save()
-            # now change renewal status
-            tmp_memb_renew.graceperiod = True
-            tmp_memb_renew.active = False
-            tmp_memb_renew.save()
+    renew_graceperiod(timezone.now())
+    membership_deactivating(timezone.now())
