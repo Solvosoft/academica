23d84e1a905d10c5a4aa4e000e52eab088bf7695
Luis Zárate M <luis.zarate@solvosoft.com>
2019-10-23 22:55:06 -0600

    Fixed docker container garbage

--- run_celery.sh
+++ run_celery.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker run  -p 8025:8025 -p  1025:1025 -d mailhog/mailhog
-docker run --name membresias-redis -p 6379:6379 -d redis 
+docker run -d --rm --name membresias-mail -p 8025:8025 -p  1025:1025 -d mailhog/mailhog
+docker run -d --rm --name membresias-redis -p 6379:6379 -d redis 
 cd src/
 celery -A membresias_codigosur worker -l info -B
+
+docker rm -f membresias-mail
+docker rm -f membresias-redis
