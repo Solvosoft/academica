1bee7c3a770220aa478481a082ef8a43a1b523b3
Kenneth Alvarez Esquivel <kenneth_alv@hotmail.com>
2019-11-05 04:47:03 -0600

    update, added some asserts

--- src/membership_manager/tests/tests_memberships.py
+++ src/membership_manager/tests/tests_memberships.py
@@ -1,36 +1,43 @@
-# from django.contrib.admin.models import LogEntry
-# from django.test import TestCase
-# from django.utils import timezone
-# from membership_manager.task_utils import membership_deactivating, renew_graceperiod, invoice_creation
-# from membership_manager.tests.tests_utils import create_contacts, add_organization, generate_active_memberships_to_graceperiod
-# from membership_manager.utils import loademailtemplates
-# # Create your tests here.
-#
-#
-# class MembershipToPay(TestCase):
-#     now = timezone.now()
-#     def setUp(self):
-#         loademailtemplates()
-#         create_contacts(2)
-#         add_organization()
-#         generate_active_memberships_to_graceperiod()
-#
-#     def test_membership_graceperiod_deactivate(self):
-#         membership_deactivating(self.now)
-#         result = LogEntry.objects.filter(object_repr='Membresia inactiva por falta de pago').count()
-#         expected = 0
-#         self.assertEqual(result,expected)
-#
-#     def test_membership_invoice_creation(self):
-#         invoice_creation(self.now)
-#         result = LogEntry.objects.filter(object_repr='Factura creada pendiente de pago').count()
-#         expected = 1
-#         self.assertEqual(result, expected)
-#
-#     def test_membership_renew_graceperiod(self):
-#         renew_graceperiod(self.now)
-#         result = LogEntry.objects.filter(object_repr='Membresia ha cambiado a periodo de prueba').count()
-#         expected = 1
-#         self.assertEqual(result,expected)
-#
-#
+from django.contrib.admin.models import LogEntry
+from django.test import TestCase
+from django.utils import timezone
+
+from membership_manager.models import Membership
+from membership_manager.task_utils import membership_deactivating, renew_graceperiod, invoice_creation
+from membership_manager.tests.utils import create_contacts, add_organization, generate_active_memberships_to_graceperiod
+from membership_manager.utils import loademailtemplates
+# Create your tests here.
+
+
+class MembershipToPay(TestCase):
+    now = timezone.now()
+    def setUp(self):
+        create_contacts(2)
+        add_organization()
+        generate_active_memberships_to_graceperiod()
+
+    def test_membership_graceperiod_deactivate(self):
+        membership_deactivating(self.now)
+        check_memb = Membership.objects.filter(state='inactive')
+        result = LogEntry.objects.filter(object_repr='Membresia inactiva por falta de pago').count()
+        expected = 0
+        self.assertEqual(result,expected)
+        self.assertEqual(check_memb,expected)
+
+    def test_membership_invoice_creation(self):
+        invoice_creation(self.now)
+        check_memb = Membership.objects.filter(state='pending')
+        result = LogEntry.objects.filter(object_repr='Factura creada pendiente de pago').count()
+        expected = 1
+        self.assertEqual(result, expected)
+        self.assertEqual(check_memb, expected)
+
+    def test_membership_renew_graceperiod(self):
+        renew_graceperiod(self.now)
+        check_memb = Membership.objects.filter(state='graceperiod')
+        result = LogEntry.objects.filter(object_repr='Membresia ha cambiado a periodo de prueba').count()
+        expected = 1
+        self.assertEqual(result,expected)
+        self.assertEqual(check_memb,expected)
+
+
--- src/membership_manager/tests/tests_memberships_notifications_expiration.py
+++ src/membership_manager/tests/tests_memberships_notifications_expiration.py
@@ -1,36 +1,36 @@
-# from async_notifications.models import EmailNotification
-# from django.contrib.admin.models import LogEntry
-# from django.contrib.auth.models import User
-# from django.test import TestCase
-#
-# # Create your tests here.
-# from django.utils import timezone
-#
-# from membership_manager.models import Invoice
-# from membership_manager.task_utils import notify_invoice_expiration
-# from membership_manager.tests.utils import create_contacts, add_organization, generate_memberships_to_notify_expiration
-#
-#
-#
-# class MembershipExpirationNotify(TestCase):
-#     now = timezone.now()
-#
-#     def setUp(self):
-#         create_contacts(2)
-#         add_organization()
-#         generate_memberships_to_notify_expiration()
-#         self.user = User.objects.create(username='admin',password='123456',email='admin@admin.com',
-#                                         is_superuser=True)
-#
-#     def test_membership_expiration_notify(self):
-#         notify_invoice_expiration(self.now)
-#         result = LogEntry.objects.filter(object_repr='Notificación de pago pendiente enviada').count()
-#         expected = 4
-#
-#         check_invoice_result = Invoice.objects.all().count()
-#         check_emails = EmailNotification.objects.filter(enqueued=True).count()
-#
-#         self.assertEqual(result, expected)
-#         self.assertEqual(check_emails, 4)
-#         self.assertEqual(check_invoice_result, expected)
-#
+from async_notifications.models import EmailNotification
+from django.contrib.admin.models import LogEntry
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+# Create your tests here.
+from django.utils import timezone
+
+from membership_manager.models import Invoice
+from membership_manager.task_utils import notify_invoice_expiration
+from membership_manager.tests.utils import create_contacts, add_organization, generate_memberships_to_notify_expiration
+
+
+
+class MembershipExpirationNotify(TestCase):
+    now = timezone.now()
+
+    def setUp(self):
+        create_contacts(2)
+        add_organization()
+        generate_memberships_to_notify_expiration()
+        self.user = User.objects.create(username='admin',password='123456',email='admin@admin.com',
+                                        is_superuser=True)
+
+    def test_membership_expiration_notify(self):
+        notify_invoice_expiration(self.now)
+        result = LogEntry.objects.filter(object_repr='Notificación de pago pendiente enviada').count()
+        expected = 4
+
+        check_invoice_result = Invoice.objects.all().count()
+        check_emails = EmailNotification.objects.filter(enqueued=True).count()
+
+        self.assertEqual(result, expected)
+        self.assertEqual(check_emails, 4)
+        self.assertEqual(check_invoice_result, expected)
+
