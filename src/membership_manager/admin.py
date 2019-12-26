8d708654fff06a05904630e4831652cf386e7178
Kenneth Alvarez Esquivel <kenneth_alv@hotmail.com>
2019-12-26 16:18:12 -0600

    arg extra ahora es 1, esto para que se pueda agregar nuevas instancias de modelos

--- src/membership_manager/admin.py
+++ src/membership_manager/admin.py
@@ -84,12 +84,12 @@ class ContactAdmin(admin.ModelAdmin):
 
 class MembershipRenewAdmin(admin.TabularInline):
     model = models.MembershipRenew
-    extra = 0
+    extra = 1
     classes = ['collapse', 'collapsed']
 
 class ServiceAdmin(admin.TabularInline):
     model = models.Service
-    extra = 0
+    extra = 1
     classes = ['collapse', 'collapsed']
 
 class MemberShipAdmin(admin.ModelAdmin):
@@ -245,7 +245,7 @@ class OrganizationAdmin(admin.ModelAdmin):
 
 class AttentionAdmin(admin.StackedInline):
     model = models.Attention
-    extra = 0
+    extra = 1
 
 class ActivityReportAdmin(admin.ModelAdmin):
     search_fields = ('start_date',)
