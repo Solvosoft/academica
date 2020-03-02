1c47007090ee72bb52e43be9f9c1b831d398d13b
Luis Zárate M <luis.zarate@solvosoft.com>
2020-03-01 18:07:43 -0600

    fixed start app telegram bot

--- src/membership_telbot_manager/apps.py
+++ src/membership_telbot_manager/apps.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from django.apps import AppConfig
 from django.conf import settings
 import telebot
@@ -8,5 +10,14 @@ class MembershipTelbotManagerConfig(AppConfig):
     def ready(self):
         super().ready()
         if not settings.DEBUG or settings.TEST_TELEGRAM:
-            bot = telebot.TeleBot(settings.TELEGRAM_BOT_API)
-            bot.set_webhook(url=settings.TELEGRAM_BOT_WEBHOOK)
+            self.initial_bot()
+
+    def initial_bot(self):
+        ok = False
+        while not ok:
+            try:
+                bot = telebot.TeleBot(settings.TELEGRAM_BOT_API)
+                bot.set_webhook(url=settings.TELEGRAM_BOT_WEBHOOK)
+                ok=True
+            except:
+                sleep(3)
