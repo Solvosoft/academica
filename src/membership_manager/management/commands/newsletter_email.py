1c379c5524fdb067187ceac0ec6e50492f351b69
Luis Zárate M <luis.zarate@solvosoft.com>
2020-05-17 21:36:26 -0600

    test configuration

---
+++ src/membership_manager/management/commands/newsletter_email.py
@@ -0,0 +1,24 @@
+from django.core.management import BaseCommand
+
+from async_notifications.newsletter_utils import get_connections_config, get_from_email
+from django.core import mail
+
+class Command(BaseCommand):
+    help = "Load templates command"
+
+    def add_arguments(self, parser):
+        parser.add_argument('email', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        messages = []
+        print(repr(options))
+        with mail.get_connection(**get_connections_config()) as connection:
+            message = mail.EmailMessage('test email server',
+                            "This is a email test from newsletter functionality",
+                                        get_from_email(),
+                                        options['email'],
+                                        connection=connection
+                                        )
+            message.content_subtype = "html"
+            messages.append(message)
+            print("Sent ", connection.send_messages(messages))
