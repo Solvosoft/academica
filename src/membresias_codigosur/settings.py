5458520309ffdd1d01f6e8d1b8d92a4fd4eee1c7
Luis Zárate M <luis.zarate@solvosoft.com>
2020-05-23 20:06:13 -0600

    changing db

--- src/membresias_codigosur/settings.py
+++ src/membresias_codigosur/settings.py
@@ -91,12 +91,8 @@ WSGI_APPLICATION = 'membresias_codigosur.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'membresias',
-        'USER': 'codigosur',
-        'PASSWORD': 'codigosur',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
