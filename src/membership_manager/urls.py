e6622beeeebe030a7ee2a321c85020e19e666722
William Alexander <william.al1379@gmail.com>
2020-09-03 10:56:51 -0600

    Add Contacts crud and reorganized template

--- src/membership_manager/urls.py
+++ src/membership_manager/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from membership_manager import views
+from membership_manager.views import ContactView
 
-
+contact_view = ContactView()
 urlpatterns = [
-    path('home/', views.index, name="home")
+    path('home/', views.index, name="home"),
+    path('', include(contact_view.get_urls()))
 ]
--- src/membership_manager/views.py
+++ src/membership_manager/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from membership_manager.models import Contact
+from djgentelella.cruds.base import CRUDView
 
 
 @login_required
 def index(request):
     return render(request, 'membresias/home.html')
+
+
+class ContactView(CRUDView):
+    model = Contact
+    template_name_base = "membresias/djgentelella/cruds"
