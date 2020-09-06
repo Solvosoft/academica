0234e8ec429ef9d5f131b8b10708a52875e963f7
William Alexander <william.al1379@gmail.com>
2020-09-06 12:05:10 -0600

    Add filter by contact name and organization name

--- src/membership_manager/views.py
+++ src/membership_manager/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from membership_manager.models import Contact, Organization, Membership
 from djgentelella.cruds.base import CRUDView
 from django.views.generic import ListView
+from django.db.models import Q
 
 
 @login_required
@@ -21,6 +22,24 @@ class OrganizationView(CRUDView):
 
 
 class MembershipListView(ListView):
-    model = Membership
     template_name = "membresias/membership_list.html"
     paginate_by = 10
+    model = Membership
+    
+    def get_context_data(self, **kwargs):
+        queryset = Membership.objects.all()
+        context = super().get_context_data(**kwargs)
+        q = self.request.GET.get('q')
+        if( q != None):
+            #need to implement countries filter but it will be overwrite soon
+            queryset = Membership.objects.filter(
+                    #Q(contact__country__in=q) | 
+                    Q(organization__name__icontains=q) |
+                    Q(contact__first_name__icontains=q) |
+                    Q(contact__last_name__icontains=q)
+                    )
+        context['memberships'] = queryset
+        context['q'] = ''
+        if q != None:
+            context["q"]=q
+        return context
