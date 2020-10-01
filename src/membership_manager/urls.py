84d16d2b71fec1b2ae208b06251767e99d7e96e0
William Alexander <william.al1379@gmail.com>
2020-09-30 21:01:02 -0600

    Add auth system membership to sidebar

--- src/membership_manager/urls.py
+++ src/membership_manager/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.urls import path
 
 from membership_manager import views
@@ -14,7 +14,8 @@ urlpatterns = [
     path('home/', views.index, name="home"),
     path('contacts/', login_required(ContactListView.as_view()), name="contacts"),
     path('organizations/', login_required(MembershipListView.as_view()), name="organizations"),
-    path('memberships/', MembershipListView.as_view(), name="memberships"),
+    path('memberships/', permission_required(
+        'membership.can_view')(MembershipListView.as_view()), name="memberships"),
     path(
         'membership/delete_service/<int:pk>/', delete_membership_service,
         name="delete_membership_service"),
