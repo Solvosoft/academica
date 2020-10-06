930587ce2ef5209a2ac16e8f073c978cec01e146
William Alexander <william.al1379@gmail.com>
2020-10-06 16:15:40 -0600

    Include admin and remove * import

--- academica/urls.py
+++ academica/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from matricula.views import index
-from matricula.views.Auth import *
+from matricula.views.Auth import get_profile
+
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'academica.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url('^$', index, name='index'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^matricula/', include('matricula.urls')),
     url(r'^matricula_bills/', include('matricula.contrib.bills.urls')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
--- matricula/urls.py
+++ matricula/urls.py
@@ -5,12 +5,15 @@ Created on 7/4/2015
 @author: luisza
 '''
 
-from django.conf.urls import url, include
-from matricula.views.Auth import *
-from matricula.views.Courses import *
+from django.conf.urls import url
+from matricula.views.Auth import recover_password,\
+    mail_recover_pass, authenticate, create_user, login_user,\
+    confirm_email, logout, StudentEdit
+from matricula.views.Courses import list_courses, view_course
 from .admin import admin_site
 from matricula.views.Pages import PageDetail
-from matricula.views.Enrollments import *
+from matricula.views.Enrollments import list_enroll, enrollme,\
+    finish_enroll
 
 urlpatterns = [
     url('^create_user$', create_user, name="create_user"),
@@ -20,16 +23,15 @@ urlpatterns = [
     url('^logout$', logout, name="logout"),
     url('^recover_password$', recover_password, name="recover_password"),
     url('^mail_recover_pass$', mail_recover_pass, name='mail_recover_pass'),
-    url('^user/profile/(?P<pk>[0-9]+)/$', StudentEdit.as_view(), name='myprofile'),
+    url('^user/profile/(?P<pk>[0-9]+)/$', StudentEdit.as_view(),
+        name='myprofile'),
     url('^courses$', list_courses, name='courses'),
-    url('^course/(?P<pk>\d+)$', view_course, name='course'),
-
-
-    url('^enrollme/(?P<pk>\d+)$', enrollme, name="enrollme"),
+    url('^course/(?P<pk>\\d+)$', view_course, name='course'),
+    url('^enrollme/(?P<pk>\\d+)$', enrollme, name="enrollme"),
     url('^enrollment$', list_enroll, name="enrollment"),
-    url('^finish_enroll/(?P<pk>\d+)$', finish_enroll, name="finish_enroll"),
+    url('^finish_enroll/(?P<pk>\\d+)$', finish_enroll, name="finish_enroll"),
 
 
-    url(r'^admin/', include(admin_site.urls)),
+    url(r'^admin/', admin_site.urls),
     url(r'^pages/(?P<pk>\d+)$', PageDetail.as_view(), name="academica_pages"),
-               ]
+]
