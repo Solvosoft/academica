51e52968857e607573b92968b4ee05bc86807062
William Alexander <william.al1379@gmail.com>
2020-10-06 16:17:00 -0600

    Add on_delete property in ForeignKey fields

--- matricula/models.py
+++ matricula/models.py
@@ -42,7 +42,8 @@ class Category(models.Model):
 
 @python_2_unicode_compatible
 class Course(models.Model):
-    category = models.ForeignKey(Category, verbose_name=_("Category"))
+    category = models.ForeignKey(
+        Category, verbose_name=_("Category"), on_delete=models.CASCADE)
     name = models.CharField(max_length=300, verbose_name=_("Name"))
     content = RichTextField(verbose_name=_("Content"))
 
@@ -69,24 +70,33 @@ class Group(models.Model):
 
     FLOWS = (
              (NORMAL, _('Normal flow (manual enroll activate)')),
-             (AUTO_PREENROLL, _("Auto pre-enroll (automatic enroll activate)")),
+             (
+                AUTO_PREENROLL,
+                _("Auto pre-enroll (automatic enroll activate)")),
              (AUTO_ENROLL, _("Auto enroll (automatic enroll finished)")),
              )
-    period = models.ForeignKey(Period, verbose_name=_("Period"))
-    course = models.ForeignKey(Course, verbose_name=_("Course"))
+    period = models.ForeignKey(
+        Period, verbose_name=_("Period"), on_delete=models.CASCADE)
+    course = models.ForeignKey(
+        Course, verbose_name=_("Course"), on_delete=models.CASCADE)
     name = models.CharField(max_length=50, verbose_name=_("Name"))
     schedule = models.CharField(max_length=300, verbose_name=_("Schedule"))
-
-    pre_enroll_start = models.DateTimeField(verbose_name=_("Pre enroll start hour"))
-    pre_enroll_finish = models.DateTimeField(verbose_name=_("Pre enroll finish hour"))
-
+    pre_enroll_start = models.DateTimeField(
+        verbose_name=_("Pre enroll start hour"))
+    pre_enroll_finish = models.DateTimeField(
+        verbose_name=_("Pre enroll finish hour"))
     enroll_start = models.DateTimeField(verbose_name=_("Enroll start hour"))
     enroll_finish = models.DateTimeField(verbose_name=_("Enroll finish hour"))
-    currency = models.CharField(max_length=3, verbose_name=_("Currency"), choices=COURRENCY_CHOICES, default="CRC")
-    cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("Course cost"))
-    maximum = models.SmallIntegerField(verbose_name=_("Maximum number of students"))
+    currency = models.CharField(
+        max_length=3, verbose_name=_("Currency"), choices=COURRENCY_CHOICES,
+        default="CRC")
+    cost = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name=_("Course cost"))
+    maximum = models.SmallIntegerField(
+        verbose_name=_("Maximum number of students"))
     is_open = models.BooleanField(default=True)
-    flow = models.SmallIntegerField(choices=FLOWS, default=NORMAL, verbose_name=_("Enrollment behavior"))
+    flow = models.SmallIntegerField(
+        choices=FLOWS, default=NORMAL, verbose_name=_("Enrollment behavior"))
 
     @property
     def in_enrollment(self):
@@ -104,12 +114,19 @@ class Group(models.Model):
 
 @python_2_unicode_compatible
 class Enroll(models.Model):
-    enroll_finished = models.BooleanField(default=False, verbose_name=_("Is enroll finished?"))
-    enroll_activate = models.BooleanField(default=False, verbose_name=_("Is active for enroll?"))
-    group = models.ForeignKey(Group, verbose_name=_("Group"))
-    student = models.ForeignKey(Student, verbose_name=_("Student"))
-    enroll_date = models.DateTimeField(auto_now_add=True, verbose_name=_("Enroll date"))
-    bill_created = models.BooleanField(default=False, verbose_name=_("Bill created"))  # is needed by bill sistem 
+    enroll_finished = models.BooleanField(
+        default=False, verbose_name=_("Is enroll finished?"))
+    enroll_activate = models.BooleanField(
+        default=False, verbose_name=_("Is active for enroll?"))
+    group = models.ForeignKey(
+        Group, verbose_name=_("Group"), on_delete=models.CASCADE)
+    student = models.ForeignKey(
+        Student, verbose_name=_("Student"), on_delete=models.CASCADE)
+    enroll_date = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("Enroll date"))
+    # bill field is needed to bill system
+    bill_created = models.BooleanField(
+        default=False, verbose_name=_("Bill created"))
 
     def __str__(self):
         return self.student.username + " -- " + smart_text(self.group)
@@ -127,18 +144,26 @@ class MenuItem(models.Model):
              (2, _("Do not used "))
              )
     name = models.CharField(max_length=50, verbose_name=_("Name"))
-    type = models.SmallIntegerField(choices=TYPES, default=0, verbose_name=_("Type"))
-    description = models.CharField(max_length=50, verbose_name=_("Description"))
-    require_authentication = models.BooleanField(default=False, verbose_name=_("Authentication is required"))
+    type = models.SmallIntegerField(
+        choices=TYPES, default=0, verbose_name=_("Type"))
+    description = models.CharField(
+        max_length=50, verbose_name=_("Description"))
+    require_authentication = models.BooleanField(
+        default=False, verbose_name=_("Authentication is required"))
     order = models.SmallIntegerField(verbose_name=_("Menu order"))
-    parent = models.ForeignKey('self', null=True, blank=True, verbose_name=_("Page parent"))
-    publicated = models.BooleanField(default=True, verbose_name=_("Publicated"))
+    parent = models.ForeignKey(
+        'self', null=True, blank=True, verbose_name=_("Page parent"),
+        on_delete=models.CASCADE)
+    publicated = models.BooleanField(
+        default=True, verbose_name=_("Publicated"))
     is_index = models.BooleanField(default=False, verbose_name=_("Index page"))
 
     def get_title_menu(self, request):
-        name = MenuTranslations.objects.filter(menu=self, language=request.LANGUAGE_CODE)
+        name = MenuTranslations.objects.filter(
+            menu=self, language=request.LANGUAGE_CODE)
         if not name:
-            name = MenuTranslations.objects.filter(menu=self, language=settings.LANGUAGE_CODE)
+            name = MenuTranslations.objects.filter(
+                menu=self, language=settings.LANGUAGE_CODE)
 
         if not name:
             name = self.description
@@ -162,7 +187,7 @@ class MenuTranslations(models.Model):
                                 default=settings.LANGUAGE_CODE,
                                 verbose_name=_("Language"))
     name = models.CharField(max_length=50, verbose_name=_("Description"))
-    menu = models.ForeignKey(MenuItem, null=True)
+    menu = models.ForeignKey(MenuItem, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -188,9 +213,7 @@ class MultilingualContent(models.Model):
                                 verbose_name=_("Language"))
     title = models.CharField(max_length=300, null=True, blank=True)
     content = RichTextField(verbose_name=_("Content"))
-    page = models.ForeignKey(Page)
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.language
-
-
