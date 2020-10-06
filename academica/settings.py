9a560e7d5e53f4d9721964c592dfeca7e1d4515f
William Alexander <william.al1379@gmail.com>
2020-10-05 22:18:55 -0600

    Update app from sqlite3 to postgresql database

--- academica/settings.py
+++ academica/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'academica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'academica',
+        'USER': 'academica',
+        'PASSWORD': 'academica',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
