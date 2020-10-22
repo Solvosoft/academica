a606b0e80503fe7d6ac6fdc19938b57ff0d4d735
Luis Zárate M <luis.zarate@solvosoft.com>
2020-10-22 09:01:22 -0600

    nuevo tipo de membresía

--- src/membership_manager/models.py
+++ src/membership_manager/models.py
@@ -87,7 +87,9 @@ class Membership(models.Model):
              ("Organizacional", "Organizacional"),
              ("Global", "Global"),
              ("Honoraria", "Honoraria"),
-             ('Básica', 'Básica'))
+             ('Básica', 'Básica'),
+             ('Streaming.la', 'Streaming.la')
+             )
     creation_date = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     membership_type = models.CharField(max_length=50, choices=TYPES, verbose_name="Tipo de membresía")
     organization = models.ForeignKey(Organization, null=True, blank=True, on_delete=models.CASCADE)
