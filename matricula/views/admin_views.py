f52b745f2bd3ced5580cbd3eb4eeccc6cd2bd826
William Alexander <william.al1379@gmail.com>
2020-10-29 13:02:12 -0600

    Remove some gramatical erros

--- matricula/views/admin_views.py
+++ matricula/views/admin_views.py
@@ -380,10 +380,10 @@ def create_period(request):
         context['form'] = form
         if form.is_valid():
             form.save()
-            messages.success(request, "Periodo guardado con éxito")
+            messages.success(request, "Período guardado con éxito")
             return HttpResponseRedirect(reverse('periods'))
         else:
-            messages.error(request, "Error al guardar el periodo")
+            messages.error(request, "Error al guardar el período")
             context['object_list'] = Period.objects.all()
             return render(request, 'periods/period_list.html', context)
     return HttpResponseRedirect(reverse('periods'))
@@ -393,7 +393,7 @@ def create_period(request):
 class PeriodDelete(DeleteView):
     model = Period
     success_url = "/matricula/enrrolment/periods"
-    success_message = "Periodo eliminado con éxito"
+    success_message = "Período eliminado con éxito"
 
     def dispatch(self, *args, **kwargs):
         """ Permission check for this class """
@@ -417,7 +417,7 @@ def edit_period(request, pk=None):
             form = PeriodCreateForm(request.POST, instance=instance)
             context['form'] = form
             if form.is_valid():
-                messages.success(request, "Periodo guardado con éxito")
+                messages.success(request, "Período guardado con éxito")
                 form.save()
                 return HttpResponseRedirect(reverse('periods'))
             else:
