2763ab7e93c811ae0bd62570f77e0c61afd5bac9
allexiusw <william.al1379@gmail.com>
2020-10-30 18:23:38 -0600

    show erolls if user has an student

--- matricula/views/Enrollments.py
+++ matricula/views/Enrollments.py
@@ -51,17 +51,23 @@ def enrollme(request, pk):
 
 @login_required
 def list_enroll(request):
-    print(request.user.pk)
-    student = Student.objects.get(pk=request.user.pk)
-    list_enroll = Enroll.objects.filter(student=student, enroll_activate=True, enroll_finished=False,
-      group__enroll_start__lte=timezone.now(),
-      group__enroll_finish__gte=timezone.now()).order_by("-enroll_date")
+    if hasattr(request.user, 'student'):
+        student = Student.objects.get(pk=request.user.pk)
+        list_enroll = Enroll.objects.filter(student=student, enroll_activate=True, enroll_finished=False,
+        group__enroll_start__lte=timezone.now(),
+        group__enroll_finish__gte=timezone.now()).order_by("-enroll_date")
 
-    finished_enroll = Enroll.objects.filter(student=student, enroll_finished=True).order_by("-enroll_date")
+        finished_enroll = Enroll.objects.filter(student=student, enroll_finished=True).order_by("-enroll_date")
 
-    return render(request, 'enroll.html', {'list_enroll': list_enroll,
-                                           'finished_enroll': finished_enroll}
-                  )
+        return render(request, 'enroll.html', {'list_enroll': list_enroll,
+                                            'finished_enroll': finished_enroll,
+                                            'student': True}
+                    )
+    else:
+        return render(request, 'enroll.html', {'list_enroll': Enroll.objects.none(),
+                                            'finished_enroll': Enroll.objects.none(),
+                                            'student': False}
+                    )
 
 
 @login_required
