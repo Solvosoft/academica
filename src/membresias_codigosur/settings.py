e9f0a1660dc9b503e2e782b6d956d222c9b89618
allexiusw <william.al1379@gmail.com>
2020-11-03 17:46:06 -0600

    Add locale language support

--- src/membresias_codigosur/settings.py
+++ src/membresias_codigosur/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 
 ]
 
