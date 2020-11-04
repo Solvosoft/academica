80416082329c72d68f29db75e1178a436722eaef
allexiusw <william.al1379@gmail.com>
2020-11-04 02:00:11 -0600

    Add ipn bills

--- src/membresias_codigosur/settings.py
+++ src/membresias_codigosur/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
+    'paypal.standard.ipn'
 ]
 
 MIDDLEWARE = [
