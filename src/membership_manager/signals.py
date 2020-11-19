2b8683073e06777d3dad70f7dc7ba80a1a9607aa
allexiusw <william.al1379@gmail.com>
2020-11-19 10:44:28 -0600

    Remove allauth signal, it isn't needed anymore

--- src/membership_manager/signals.py
+++ src/membership_manager/signals.py
@@ -1,17 +1,7 @@
-from django.utils.translation import ugettext_lazy as _
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
-from membership_manager.models import Membership, ActivityReport, Invoice, MembershipRenew
+from membership_manager.models import Membership, ActivityReport, Invoice
 from membership_manager.task_utils import send_welcome_notification
-from membership_manager.tasks import task_create_invoice
-from allauth.account.signals import user_signed_up
-from matricula.models import Student
-from django.utils.timezone import now
-from django.template.loader import render_to_string
-from django.core.mail import send_mail
-from django.conf import settings
-from django.urls import reverse
-
 
 
 @receiver(post_save, sender=Membership)
@@ -28,8 +18,10 @@ def welcome_email(sender, instance, created, **kwargs):
 @receiver(post_save, sender=ActivityReport)
 def save_attention(sender, instance, **kwargs):
 
-    if kwargs['update_fields'] is not None and 'duration' in kwargs['update_fields']:
-        instance.__class__.objects.filter(pk=instance.pk).update(manual_edited=True)
+    if kwargs['update_fields'] is not None and\
+            'duration' in kwargs['update_fields']:
+        instance.__class__.objects.filter(
+            pk=instance.pk).update(manual_edited=True)
 
     if not instance.manual_edited or instance.duration == 0:
         instance.manual_edited = False
@@ -38,25 +30,11 @@ def save_attention(sender, instance, **kwargs):
             d = act.end_date - act.start_date
             total += d.days*24 + d.seconds / 60 / 60
         instance.duration = total
-        instance.__class__.objects.filter(pk=instance.pk).update(duration=int(total))
+        instance.__class__.objects.filter(
+            pk=instance.pk).update(duration=int(total))
 
 
 @receiver([pre_delete], sender=Invoice)
-def remove_invoice_pdf(sender, instance, using,**kwargs):
+def remove_invoice_pdf(sender, instance, using, **kwargs):
     if instance.pdf_invoice:
         instance.pdf_invoice.delete(False)
-
-
-@receiver(user_signed_up)
-def user_signed_up_(request, user, **kwargs):
-    student = Student(user=user, created_at=now(), confirmed_at=now())
-    student.save()
-    mail_body = render_to_string("email_welcome.html",
-        {
-        "url": request.build_absolute_uri(reverse('courses')),
-        "user": user,
-        })
-    send_mail(_('Email confirmation'),
-        'Url confirmation %s' % (request.build_absolute_uri(reverse('courses'))),
-        settings.DEFAULT_FROM_EMAIL, [user.email],
-        html_message=mail_body)
