4a2cedf758c24fe26af9ed9120a0f6fb13f73b5c
marcegz <marcegz31@gmail.com>
2020-11-23 16:07:28 -0600

    Incluye titulos en las acciones de los listados

--- src/matricula/views/admin_views.py
+++ src/matricula/views/admin_views.py
@@ -4,42 +4,43 @@ Created on 18/10/2020
 
 @author: allexiusw
 '''
-from django.conf import settings
-from django.contrib.admin.views.decorators import staff_member_required
-from django.views.generic import ListView, DeleteView
-from django.utils.translation import ugettext_lazy as _
-from django.shortcuts import render, redirect
-from matricula.models import Category, Course, Period, Group,\
-    Enroll, Student, Page, Professor
-from matricula.forms import CategoryCreateForm, CategorySearchForm,\
-    CourseSearchForm, CourseCreateForm, MenuItemSearchForm,\
-    MenuItemCreateForm, PeriodCreateForm, PeriodSearchForm, GroupCreateForm,\
-    GroupSearchForm, EnrollSearchForm, EnrollCreateForm, StudentSearchForm,\
-    StudentAdminCreateForm, PageCreateForm, PageSearchForm, MenuItemAddForm,\
-    PreEnrollAddGroupForm, GroupAddForm, GroupEditForm, PermissionForm
-from djgentelella.models import MenuItem as DJMenuItem
-from django.contrib import messages
-from django.urls import reverse
-from django.http import HttpResponseRedirect
-from django.contrib.auth.decorators import permission_required
-from django.utils.decorators import method_decorator
-from django.db.models import Q
-from django.contrib.auth.models import User
-from django.core.paginator import Paginator
 import csv
-from django.http import HttpResponse
-from django.utils.timezone import now
-from .utils import get_active_period
-from xhtml2pdf import pisa
-from django.shortcuts import get_object_or_404
-from django.template.loader import get_template
-from django.contrib.staticfiles import finders
-import os
-from matricula.views.utils import get_expire_date
-from async_notifications.utils import send_email_from_template
 import io
+import os
+
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.models import User
+from django.contrib.staticfiles import finders
 from django.core.files.base import File
-from django.core.files.base import File
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render, redirect
+from django.template.loader import get_template, render_to_string
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.utils.timezone import now
+from django.utils.translation import ugettext_lazy as _
+from django.views.generic import ListView, DeleteView
+from djgentelella.models import MenuItem as DJMenuItem
+from xhtml2pdf import pisa
+
+from async_notifications.utils import send_email_from_template
+from matricula.forms import CategoryCreateForm, CategorySearchForm, \
+    CourseSearchForm, CourseCreateForm, MenuItemSearchForm, \
+    MenuItemCreateForm, PeriodCreateForm, PeriodSearchForm, GroupCreateForm, \
+    GroupSearchForm, EnrollSearchForm, EnrollCreateForm, StudentSearchForm, \
+    StudentAdminCreateForm, PageCreateForm, PageSearchForm, MenuItemAddForm, \
+    PreEnrollAddGroupForm, GroupAddForm, GroupEditForm, PermissionForm
+from matricula.models import Category, Course, Period, Group, \
+    Enroll, Student, Page, Professor
+from matricula.views.utils import get_expire_date
+from .utils import get_active_period
 
 
 def link_callback(uri, rel):
