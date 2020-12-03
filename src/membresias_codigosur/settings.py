c86b92ddc667a67738e5a71396d46b1362897a95
allexiusw <william.al1379@gmail.com>
2020-12-03 16:45:06 -0600

    Remove froala upload path

--- src/membresias_codigosur/settings.py
+++ src/membresias_codigosur/settings.py
@@ -167,7 +167,6 @@ LOCALE_PATHS = (
     os.path.join(BASE_NOCODE_DIR, 'locale/'),
 )
 TINYMCE_UPLOAD_PATH =os.path.join(MEDIA_ROOT, 'tinymce')
-FROALA_UPLOAD_PATH = os.path.join(MEDIA_ROOT, 'froala_editor/')
 
 # python -m smtpd -c DebuggingServer -n localhost:1025
 # o también ver https://github.com/mailhog/MailHog
