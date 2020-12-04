4c5c95bbe4c25ff1b364c17bb673558e33948ccb
allexiusw <william.al1379@gmail.com>
2020-12-04 13:55:00 -0600

    Remove menuitem when deleting a page

--- src/matricula/views/admin_views.py
+++ src/matricula/views/admin_views.py
@@ -1168,9 +1168,9 @@ class PageDelete(DeleteView):
 
     def delete(self, request, *args, **kwargs):
         page = self.get_object()
-        menu = DJMenuItem.objects.get(url_name="/pages/"+page.slug)
-        if menu:
-            menu.delete()
+        menu = DJMenuItem.objects.filter(url_name="/pages/"+page.slug).all()
+        for mn in menu:
+            mn.delete()
         messages.success(self.request, self.success_message)
         return super(PageDelete, self).delete(request, *args, **kwargs)
 
