810e586817afd90ce951e326dfe16f1e82ed5eb1
allexiusw <william.al1379@gmail.com>
2020-12-23 09:40:30 -0600

    Allow Decimal values in fees field

--- src/membership_manager/forms.py
+++ src/membership_manager/forms.py
@@ -113,7 +113,7 @@ class MembershipForm(GTForm, forms.ModelForm):
             'renewal_period': widget.Select,
             'apply_fees': widget.YesNoInput,
             'state': widget.Select,
-            'fees': widget.NumberInput,
+            'fees': forms.NumberInput(attrs={'step': "0.01", "class": "form-control"}),
             'free_membership': widget.YesNoInput
         }
 
