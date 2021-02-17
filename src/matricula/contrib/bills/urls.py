c482252b2c1253ea0620edb687a3bf017468fe74
allexiusw <william.al1379@gmail.com>
2021-02-17 12:23:37 -0600

    Change main text sidebar to UPO according to path

--- src/matricula/contrib/bills/urls.py
+++ src/matricula/contrib/bills/urls.py
@@ -12,14 +12,14 @@ from .views.admin_views import ColonExchangeList, create_colonexchange, ColonExc
     edit_colonexchange, BillList, create_bill, edit_bill, BillDelete
 
 urlpatterns = [
-    url('^bills/$', get_my_bills, name="bills"),
-    url(r'^paybills/paypal/', include('paypal.standard.ipn.urls')),
-    url('list', BillList.as_view(), name="listbills"),
-    url('create_bill', create_bill, name="create_bill"),
-    path('delete_bill/<int:pk>/', BillDelete.as_view() , name="delete_bill"),
-    path('edit_bill/<int:pk>/', edit_bill, name="edit_bill"),
-    url('colonexchanges', ColonExchangeList.as_view(), name="colonexchange"),
-    url('create_colonexchange', create_colonexchange, name="create_colonexchange"),
-    path('delete_colonexchange/<int:pk>/', ColonExchangeDelete.as_view() , name="delete_colonexchange"),
-    path('edit_colonexchange/<int:pk>/', edit_colonexchange, name="edit_colonexchange"),
+    url('bills/$', get_my_bills, name="bills"),
+    url(r'^bills/paybills/paypal/', include('paypal.standard.ipn.urls')),
+    url('bills/list', BillList.as_view(), name="listbills"),
+    url('bills/create_bill', create_bill, name="create_bill"),
+    path('bills/delete_bill/<int:pk>/', BillDelete.as_view() , name="delete_bill"),
+    path('bills/edit_bill/<int:pk>/', edit_bill, name="edit_bill"),
+    url('bills/colonexchanges', ColonExchangeList.as_view(), name="colonexchange"),
+    url('bills/create_colonexchange', create_colonexchange, name="create_colonexchange"),
+    path('bills/delete_colonexchange/<int:pk>/', ColonExchangeDelete.as_view() , name="delete_colonexchange"),
+    path('bills/edit_colonexchange/<int:pk>/', edit_colonexchange, name="edit_colonexchange"),
 ]
