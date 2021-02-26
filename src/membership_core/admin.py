39c7454d5916a169a8d29bc20d6ad894f6ec2998
William Alexander <william.al1379@gmail.com>
2021-02-26 16:57:42 -0600

    Remove unused models from membership_core

--- src/membership_core/admin.py
+++ src/membership_core/admin.py
@@ -1,26 +1,6 @@
 from django.contrib import admin
-from membership_core import models
+from membership_core.models import SystemCurrency, Country
 
-# Register your models here.
-admin.site.register([
-    models.SystemCurrency, models.ServiceType, models.RenewalPeriod,
-    models.Country
-])
 
-class ServiceMT(admin.TabularInline):
-    model = models.ServiceMT
-    extra = 1
-    classes = ['collapse', 'collapsed']
-
-class MembershipTemplateAdmin(admin.ModelAdmin):
-    list_filter = ('state',)
-    search_fields = ('name', 'currency')
-    list_display = ('name', 'annual_cost', 'currency', 'renewal_period', 'state')
-    inlines = [ServiceMT,]
-
-admin.site.register(models.MembershipTemplate, MembershipTemplateAdmin)
-admin.site.site_title = "Membresias de Código Sur"
-admin.site.site_header = "Membresias de Código Sur"
-admin.site.index_title = "Membresias de Código Sur"
+admin.site.register([SystemCurrency, Country])
 admin.site.index_template =  'admin/dashboard/welcome.html'
-
