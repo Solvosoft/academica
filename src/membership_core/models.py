016e59b6401372d9bafdaab9adcf02111f19bb6a
William Alexander <william.al1379@gmail.com>
2021-02-26 20:06:08 -0600

    Update models in membership core to support trans

--- src/membership_core/models.py
+++ src/membership_core/models.py
@@ -1,24 +1,28 @@
 from django.db import models
 from .currency import CURRENCY_CHOICES
 DECIMAL_PLACES=2
+from django.utils.translation import gettext as _
 
 
 class SystemCurrency(models.Model):
     currency = models.CharField(max_length=4,
                                 choices=CURRENCY_CHOICES,
-                                verbose_name="Moneda")
+                                verbose_name=_("Currency"))
     rates = models.DecimalField(
         max_digits=10,
         decimal_places=DECIMAL_PLACES,
-        default=1, verbose_name="Tipo de cambio",
-                              help_text="1 USD equivale a X en moneda local")
+        default=1, verbose_name=_("Change type"),
+                              help_text=_("1 USD is equal to X in local currency"))
 
     def __str__(self):
         return self.currency
 
     class Meta:
-        verbose_name = "Moneda"
-        verbose_name_plural = "Monedas"
+        verbose_name = _("Currency")
+        verbose_name_plural = _("Currencies")
+        permissions = [
+            ("can_show_dashboard", _("Can show dashboard")),
+        ]
 
 
 class Country(models.Model):
@@ -28,3 +32,7 @@ class Country(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _("Country")
+        verbose_name_plural = _("Countries")
