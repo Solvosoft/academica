a601e8a95f2db9821f8909e7856ed327355bfb4d
William Alexander <william.al1379@gmail.com>
2021-03-26 14:16:26 -0600

    Enforce password validation in admin dashboard

--- src/matricula/forms.py
+++ src/matricula/forms.py
@@ -77,10 +77,15 @@ class StudentCreateForm(GTForm, forms.ModelForm):
         # At least one letter and one non-letter
         first_isalpha = password[0].isalpha()
         if all(c.isalpha() == first_isalpha for c in password):
-            raise forms.ValidationError("El password debe tener al menos una letra minúscula, un dígito y un caracter de puntuación.")
+            raise forms.ValidationError("El password debe tener al menos una letra minúscula y un dígito.")
 
         if re.search('[A-Z]', password)==None:
-            raise forms.ValidationError("El passwod debe tener al menos una letra mayúscula.")
+            raise forms.ValidationError("El password debe tener al menos una letra mayúscula.")
+
+        if not re.findall('[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password):
+            raise forms.ValidationError(
+                _("The password must contain at least 1 symbol: ")+"()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"
+            )
 
         return password
 
@@ -540,12 +545,36 @@ class StudentAdminCreateForm(GTForm, forms.ModelForm):
 
 class StudentChangePasswordForm(GTForm, forms.ModelForm):
 
+    MIN_LENGTH = 8
+
     class Meta:
         model = User
         fields = ['password']
         widgets = {
             'password': djgentelella.PasswordInput
         }
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+
+        # At least MIN_LENGTH long
+        if len(password) < self.MIN_LENGTH:
+            raise forms.ValidationError("El password debe tener al menos %d caracteres de longitud." % self.MIN_LENGTH)
+
+        # At least one letter and one non-letter
+        first_isalpha = password[0].isalpha()
+        if all(c.isalpha() == first_isalpha for c in password):
+            raise forms.ValidationError("El password debe tener al menos una letra minúscula y un dígito.")
+
+        if re.search('[A-Z]', password)==None:
+            raise forms.ValidationError("El password debe tener al menos una letra mayúscula.")
+
+        if not re.findall('[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password):
+            raise forms.ValidationError(
+                _("The password must contain at least 1 symbol: ")+"()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"
+            )
+
+        return password
 
 class PageSearchForm(GTForm, forms.Form):
     slug = forms.CharField(
