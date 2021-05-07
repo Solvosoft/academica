f7cd69887634e667753257e6ee96688476777da3
William Alexander <william.al1379@gmail.com>
2021-05-06 19:11:36 -0600

    Add username when reset password from admin

--- src/matricula/management/commands/update_academy_temp.py
+++ src/matricula/management/commands/update_academy_temp.py
@@ -9,10 +9,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         templates = [
             'set_email_first_academy',
+            'email_recovery_academy',
         ]
+
         EmailTemplate.objects.filter(code__in=templates).delete()
         TemplateContext.objects.filter(code__in=templates).delete()
+
         update_template_context(
             'set_email_first_academy', 'Sólo un paso más para registrarte - UPo.',
             [('url'), ('student'),("domain")], 'set_email_first.html',
             as_template=True)
+
+        update_template_context(
+        'email_recovery_academy', 'Correo de recuperación de contraseña',
+        [('user'), ('student'), ('url'), ("domain")], 'email_recovery.html',
+        as_template=True)
