4a94671a89fe454657f2080bbb24dbfdf836c23a
William Alexander <william.al1379@gmail.com>
2021-05-25 14:55:29 -0600

    Set enroll_finished in waiting list

--- src/matricula/views/admin_views.py
+++ src/matricula/views/admin_views.py
@@ -1140,7 +1140,8 @@ class GroupDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = EnrollCreateForm(initial={'group': self.get_object()})
+        context['form'] = EnrollCreateForm(
+            initial={'group': self.get_object(), 'enroll_finished':True})
         return context
     
     def post(self, request, *args, **kwargs):
