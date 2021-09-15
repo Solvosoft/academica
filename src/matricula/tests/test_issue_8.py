aafc9dc9c7964c7a48c91441b1b70d103369487a
mariovaargas <mariovargasc97@gmail.com>
2021-09-15 00:07:57 -0600

    Added issue 1 refactoring

--- src/matricula/tests/test_issue_8.py
+++ src/matricula/tests/test_issue_8.py
@@ -167,7 +167,7 @@ class Countries_In_Courses_TestCase(TestCase):
         data = response.json()['data']
         dataset = data['datasets']
 
-        self.assertEqual(dataset[0]['data'], [2, 1, 1])
+        self.assertEqual(dataset[0]['data'], [1, 1, 2])
 
 
     def test_graph_type(self):
@@ -188,4 +188,4 @@ class Countries_In_Courses_TestCase(TestCase):
         response = self.client.get(self.url)
 
         data = response.json()['data']
-        self.assertEqual(data['labels'], ['Costa Rica', 'Australia', 'Afganistán'])
+        self.assertEqual(data['labels'], ['Afganistán', 'Australia', 'Costa Rica'])
